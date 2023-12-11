396f0228f8bc495a892ee81f394f69b2aeb0b88d
Daniele Rolli <danielerolli004@gmail.com>
2023-12-11 15:58:07 +0100

    🍻 beaver-notes.rb

--- Casks/beaver-notes.rb
+++ Casks/beaver-notes.rb
@@ -1,6 +1,6 @@
 cask "beaver-notes" do
   version "2.2.2"
-  sha256 "c5298c646c5a15b202cedca70589fbf7ececa2fa597cbe4ef428bfa5bd0c0cf4"
+  sha256 "b2fa034786372b69a5a85aff0307f94677ae5d0b748fb05fc5f1a8f328c0b64d"
 
     url "https://github.com/Daniele-rolli/Beaver-Notes/releases/download/#{version}/Beaver-Notes-#{version}.dmg"
   name "Beaver Notes"
