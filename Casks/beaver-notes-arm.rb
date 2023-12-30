2169c99ad3a282213436dc0bfb9c6f8e05328fb5
Daniele Rolli <danielerolli004@gmail.com>
2023-12-30 22:24:35 +0100

    🍻 Version 2.3.0

--- Casks/beaver-notes-arm.rb
+++ Casks/beaver-notes-arm.rb
@@ -1,11 +1,8 @@
 cask "beaver-notes-arm" do
-<<<<<<< Updated upstream
-  version "2.2.2"
-  sha256 "66b6649c0152008812c288dbf146482bce3b86548737e30a48f0b8c48b548a74"
-=======
+
   version "2.3.0"
   sha256 "ded5fb91e32db97d96a468ffcd86b009b7ab5eaac9f04f7df87d6bf871608158"
->>>>>>> Stashed changes
+
 
   url "https://github.com/Daniele-rolli/Beaver-Notes/releases/download/#{version}/Beaver-notes-#{version}-arm64.dmg"
   name "Beaver Notes"
