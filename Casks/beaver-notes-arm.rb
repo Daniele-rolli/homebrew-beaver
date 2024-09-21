5b54ddb80cceaafd909f98032569f2b270548dc1
Daniele Rolli <danielerolli@proton.me>
2024-09-21 15:09:29 +0200

    🍺 Version 3.7.0

--- Casks/beaver-notes-arm.rb
+++ Casks/beaver-notes-arm.rb
@@ -1,15 +1,18 @@
-cask "beaver-notes-arm" do
-
+cask "beaver-notes-arm64" do
   version "3.6.0"
   sha256 "50671052c7ce0d28085d27e324726b92bf20e6bdfb350cf64c5f4debf8ac5232"
 
-
   url "https://github.com/Beaver-Notes/Beaver-Notes/releases/download/#{version}/Beaver-notes-arm64.dmg"
-  name "Beaver Notes"
-  desc "Your Personal Note-Taking Haven for Privacy 
-and Efficiency"
+  name "Beaver Notes (ARM64)"
+  desc "Your Personal Note-Taking Haven for Privacy and Efficiency"
   homepage "https://beavernotes.com"
-  depends_on arch: :arm64
 
+  caveats <<~EOS
+    This version is deprecated and will no longer be updated.
+    Please switch to the universal version:
+      brew reinstall --cask beaver-notes
+  EOS
+
+  depends_on arch: :arm64
   app "Beaver-notes-arm64.app"
 end
--- Casks/beaver-notes.rb
+++ Casks/beaver-notes.rb
@@ -1,15 +1,24 @@
 cask "beaver-notes" do
+  version "3.7.0"
+  sha256 "b4bed569ce8a0e1fa120fcfc8ecdec335758dfd77a27526252350777bca434a6"
 
-  version "3.6.0"
-  sha256 "b5d72e4ef53babab483261bbb56c3d4ead2001adc56153c030edc59b4d90829a"
-
-    url "https://github.com/Beaver-Notes/Beaver-Notes/releases/download/#{version}/Beaver-Notes.dmg"
+  url "https://github.com/Beaver-Notes/Beaver-Notes/releases/download/#{version}/Beaver-Notes-universal.dmg"
   name "Beaver Notes"
-  desc "Your Personal Note-Taking Haven for Privacy
-and Efficiency"
+  desc "Your Personal Note-Taking Haven for Privacy and Efficiency"
   homepage "https://beavernotes.com"
-  depends_on arch: :intel
 
-  app "Beaver-notes.app"
+  conflicts_with cask: "beaver-notes-arm64"
+
+  zap trash: [
+    "~/Applications/Beaver-notes-arm64.app",
+    "~/Library/Preferences/com.beavernotes.arm64.plist"
+  ]
+
+  app "Beaver-Notes.app"
+
+  postflight do
+    if File.exist?("/Applications/Beaver-notes-arm64.app")
+      system_command "/bin/rm", args: ["-rf", "/Applications/Beaver-notes-arm64.app"]
+    end
+  end
 end
-
